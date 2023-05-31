73784e5563a508254cb3ca082cd351a50180ef35
Mikel Lonbide <100244220+Mikeloon@users.noreply.github.com>
2023-05-31 09:30:43 +0200

    Update CustomerControllerTest.java

--- src/test/java/bookstore/controller/CustomerControllerTest.java
+++ src/test/java/bookstore/controller/CustomerControllerTest.java
@@ -34,134 +34,73 @@ public class CustomerControllerTest {
 
     @InjectMocks
     private CustomerController customerController;
-
+    
     @Mock
     private CustomerService customerService;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Mock
-    private CustomerRepository customerRepository;
-
-    @Before
-    public void init() {
-        mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+    
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.initMocks(this);
     }
-
-
+    
+    // START: Test method for getting all customers
     @Test
-    public void addCustomer() throws Exception {
-        Customer customer = new Customer(1, "94052355879", "Andrzej", "Góra");
-
-        given(customerService.addCustomer(customer)).willReturn(customer);
-        this.mockMvc.perform(post("/customer/addCustomer")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(toJson(customer)))
-                .andDo(print())
-                .andExpect(status().isOk());
+    void testGetAllCustomers() {
+        List<Customer> customers = Arrays.asList(
+                new Customer(1L, "John Doe", "john@example.com"),
+                new Customer(2L, "Jane Smith", "jane@example.com")
+        );
+        when(customerService.getAllCustomers()).thenReturn(customers);
+        ResponseEntity<List<Customer>> response = customerController.getAllCustomers();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(customers, response.getBody());
     }
-
+    // FINAL
+    
+    // START: Test method for getting a customer by ID
     @Test
-    public void findCustomerById() throws Exception {
-
-        Customer customer = new Customer(1, "94052355879", "Andrzej", "Góra");
-
-        when(customerService.findById(1)).thenReturn(Optional.of(customer));
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/customer/findCustomerById/1")).andExpect(MockMvcResultMatchers.status().isOk()).
-                andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1));
+    void testGetCustomerById() {
+        Long customerId = 1L;
+        Customer customer = new Customer(customerId, "John Doe", "john@example.com");
+        when(customerService.getCustomerById(customerId)).thenReturn(customer);
+        ResponseEntity<Customer> response = customerController.getCustomerById(customerId);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(customer, response.getBody());
     }
-
+    // FINAL
+    
+    // START: Test method for adding a new customer
     @Test
-    public void deleteCustomer() throws Exception {
-
-        List<Customer> customers = new ArrayList<>();
-        Customer customer = new Customer(1, "94052355879", "Andrzej", "Góra");
-        Customer customer2 = new Customer(2, "88052355879", "Karol", "Król");
-
-        customers.add(customer2);
-
-        when(customerService.delete(1)).thenReturn(customers);
-
-        mockMvc.perform(MockMvcRequestBuilders.delete("/customer/delete/1")).andExpect(MockMvcResultMatchers.status().isOk());
+    void testAddCustomer() {
+        Customer customer = new Customer(1L, "New Customer", "new@example.com");
+        when(customerService.addCustomer(customer)).thenReturn(customer);
+        ResponseEntity<Customer> response = customerController.addCustomer(customer);
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(customer, response.getBody());
     }
-
+    // FINAL
+    
+    // START: Test method for updating an existing customer
     @Test
-    public void updateCustomer() {
+    void testUpdateCustomer() {
+        Long customerId = 1L;
+        Customer customer = new Customer(customerId, "Updated Customer", "updated@example.com");
+        when(customerService.updateCustomer(customerId, customer)).thenReturn(customer);
+        ResponseEntity<Customer> response = customerController.updateCustomer(customerId, customer);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(customer, response.getBody());
     }
-
+    // FINAL
+    
+    // START: Test method for deleting a customer
     @Test
-    public void findCustomerByName() throws Exception {
-
-        List<Customer> customers = new ArrayList<>();
-        Customer customer = new Customer(1, "94052355879", "Andrzej", "Góra");
-        Customer customer2 = new Customer(2, "88052355879", "Karol", "Król");
-        customers.add(customer);
-        customers.add(customer2);
-
-        List<Customer> customerList = new ArrayList<>();
-        customerList.add(customer2);
-        when(customerService.findCustomerByName("Karol")).thenReturn(customerList);
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/customer/findCustomersByName/Karol")).andExpect(MockMvcResultMatchers.status().
-                isOk()).andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Karol"));
-
+    void testDeleteCustomer() {
+        Long customerId = 1L;
+        doNothing().when(customerService).deleteCustomer(customerId);
+        ResponseEntity<Void> response = customerController.deleteCustomer(customerId);
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        assertNull(response.getBody());
+        verify(customerService, times(1)).deleteCustomer(customerId);
     }
-
-    @Test
-    public void findCustomerContainsAKAndHaveMoreThan2Books() throws Exception {
-
-        List<Book> books = new ArrayList<>();
-        books.add(new Book(1, "Królewna Sniezka", 50));
-        books.add(new Book(2, "Kot w Butach", 20));
-        books.add(new Book(1, "Calineczka", 15));
-        List<Customer> customers = new ArrayList<>();
-        Customer customer = new Customer(1, "94052355879", "JAKUB", "Góra", books);
-        Customer customer2 = new Customer(2, "88052355879", "Karol", "Król", null);
-
-        List<Customer> newList = new ArrayList<>();
-        newList.add(customer);
-
-        when(customerService.findCustomerWhoseNameContainsAKAndHaveMoreThan2Books(customers)).thenReturn(newList);
-        mockMvc.perform(MockMvcRequestBuilders.get("/customer/customersWithAKAAndMoreThan2Books")).andExpect(MockMvcResultMatchers.status().isOk());
-    }
-
-    @Test
-    public void findTheOldestCustomer() throws Exception {
-        List<Customer> customers = new ArrayList<>();
-        Customer customer = new Customer(1, "94052355879", "JAKUB", "Góra", null);
-        Customer customer2 = new Customer(2, "88052355879", "Karol", "Król", null);
-
-        customers.add(customer);
-        customers.add(customer2);
-
-        // when(customerService.findTheOldestCustomer(customers)).thenReturn(customer2);
-        mockMvc.perform(MockMvcRequestBuilders.get("/customer/findTheOldestCustomer")).andExpect(MockMvcResultMatchers.status().isOk());
-
-
-    }
-
-    @Test
-    public void customersSortedBySurname() throws Exception {
-        List<Customer> customers = new ArrayList<>();
-        Customer customer = new Customer(1, "94052355879", "JAKUB", "Zalewski", null);
-        Customer customer2 = new Customer(2, "88052355879", "Karol", "Król", null);
-
-        customers.add(customer);
-        customers.add(customer2);
-
-        List<Customer> newList = new ArrayList<>();
-        newList.add(customer2);
-        newList.add(customer);
-
-        when(customerService.customersSortedBySurname()).thenReturn(newList);
-        mockMvc.perform(MockMvcRequestBuilders.get("/customer/customersSortedBySurname")).andExpect(MockMvcResultMatchers.status().isOk());
-    }
-
-    private String toJson(Customer customer) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(customer);
-    }
+    // FINAL
 }
