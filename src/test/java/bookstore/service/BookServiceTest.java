4af85b4c963b3bdd5bbdbd9d55b7f54b1775eb36
Mikel Lonbide <100244220+Mikeloon@users.noreply.github.com>
2023-05-31 09:33:48 +0200

    Update BookServiceTest.java

--- src/test/java/bookstore/service/BookServiceTest.java
+++ src/test/java/bookstore/service/BookServiceTest.java
@@ -21,119 +21,70 @@ import static org.junit.Assert.assertEquals;
 @RunWith(MockitoJUnitRunner.class)
 public class BookServiceTest {
 
-    @InjectMocks
+   @InjectMocks
     private BookService bookService;
-
+    
     @Mock
     private BookRepository bookRepository;
-
-    @Test
-    public void addBook() throws CustomerNotFoundException {
-        Book book = new Book(1, "Basnie", 22, null);
-
-        Mockito.when(bookRepository.save(book)).thenReturn(book);
-        assertEquals(book, bookService.addBook(book));
+    
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.initMocks(this);
     }
-
+    
+    // START: Test method for getting all books
     @Test
-    public void findBookById() {
-        List<Book> books = new ArrayList<>();
-        books.add(new Book(1, "Królewna Sniezka", 50, null));
-        books.add(new Book(2, "Kot w Butach", 20, null));
-
-        Book book1 = new Book(1, "Królewna Sniezka", 50, null);
-        Optional<Book> book = Optional.of(book1);
-
-        Mockito.when(bookRepository.findById(1)).thenReturn(book);
-        assertEquals(book, bookService.findBookById(1));
+    void testGetAllBooks() {
+        List<Book> books = Arrays.asList(
+                new Book(1L, "Book 1", "Author 1"),
+                new Book(2L, "Book 2", "Author 2")
+        );
+        when(bookRepository.findAll()).thenReturn(books);
+        List<Book> result = bookService.getAllBooks();
+        assertEquals(books, result);
     }
-
+    // FINAL
+    
+    // START: Test method for getting a book by ID
     @Test
-    public void delete() {
-        Book book = new Book(2, "Kot w Burach", 20, null);
-        List<Book> books = new ArrayList<>();
-        books.add(new Book(1, "Calineczka", 15, null));
-        books.add(new Book(2, "Calineczka", 20, null));
-
-        List<Book> newList = new ArrayList<>();
-        newList.add(new Book(2, "Calineczka", 20, null));
-
-        //  assertEquals(newList, bookService.delete(2));
-        Mockito.when(bookRepository.findAll()).thenReturn(newList);
-        assertEquals(newList, bookService.delete(1));
+    void testGetBookById() {
+        Long bookId = 1L;
+        Book book = new Book(bookId, "Book 1", "Author 1");
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+        Book result = bookService.getBookById(bookId);
+        assertEquals(book, result);
     }
-
+    // FINAL
+    
+    // START: Test method for adding a new book
     @Test
-    public void updateBook() throws BookNotFoundException {
-
-        Book book = new Book(2, "Kot w Burach", 20, null);
-        Optional<Book> optionalBook = Optional.of(book);
-
-        Book newBook = new Book(2, "Kot", 15, null);
-        Optional<Book> optionalNewBook = Optional.of(newBook);
-
-        Mockito.when(bookRepository.findById(2)).thenReturn(optionalNewBook);
-        assertEquals(book, bookService.updateBook(book));
-
+    void testAddBook() {
+        Book book = new Book(1L, "New Book", "New Author");
+        when(bookRepository.save(book)).thenReturn(book);
+        Book result = bookService.addBook(book);
+        assertEquals(book, result);
     }
-
+    // FINAL
+    
+    // START: Test method for updating an existing book
     @Test
-    public void findByTitle() {
-        List<Book> books = new ArrayList<>();
-        books.add(new Book(1, "Calineczka", 15, null));
-        books.add(new Book(2, "Calineczka", 20, null));
-
-        Mockito.when(bookRepository.findAllByTitle("Calineczka")).thenReturn(books);
-        assertEquals(books, bookService.findByTitle("Calineczka"));
+    void testUpdateBook() {
+        Long bookId = 1L;
+        Book book = new Book(bookId, "Updated Book", "Updated Author");
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+        when(bookRepository.save(book)).thenReturn(book);
+        Book result = bookService.updateBook(bookId, book);
+        assertEquals(book, result);
     }
-
+    // FINAL
+    
+    // START: Test method for deleting a book
     @Test
-    public void findBooksAbovePrice() {
-        List<Book> books = new ArrayList<>();
-        books.add(new Book(1, "Lalka", 15, null));
-        books.add(new Book(2, "Wojna", 20, null));
-        books.add(new Book(3, "Ogniem i mieczem", 10, null));
-
-        Mockito.when(bookRepository.findAllByPriceGreaterThan(10)).thenReturn(books);
-        assertEquals(books, bookService.findBooksAbovePrice(10));
-
+    void testDeleteBook() {
+        Long bookId = 1L;
+        doNothing().when(bookRepository).deleteById(bookId);
+        bookService.deleteBook(bookId);
+        verify(bookRepository, times(1)).deleteById(bookId);
     }
-
-    @Test
-    public void findTheCheapestBook() {
-        List<Book> books = new ArrayList<>();
-        books.add(new Book(1, "Lalka", 10, null));
-        books.add(new Book(2, "Wojna", 20, null));
-        books.add(new Book(3, "Ogniem i mieczem", 15, null));
-
-        Book book = new Book(1, "Lalka", 10, null);
-
-        assertEquals(book, bookService.findTheCheapestBook(books));
-
-
-    }
-
-    @Test
-    public void booksSortedByPrice() {
-        List<Book> books = new ArrayList<>();
-        books.add(new Book(1, "Lalka", 25, null));
-        books.add(new Book(2, "Wojna", 20, null));
-        books.add(new Book(3, "Ogniem i mieczem", 10, null));
-
-        Book book = new Book(1, "Lalka", 25, null);
-
-        Mockito.when(bookRepository.findAllByOrderByPrice()).thenReturn(books);
-        assertEquals(books, bookService.booksSortedByPrice());
-
-    }
-
-    @Test
-    public void countBooksByPriceAfter() {
-        List<Book> books = new ArrayList<>();
-        books.add(new Book(1, "Lalka", 25, null));
-        books.add(new Book(2, "Wojna", 20, null));
-
-        Mockito.when(bookRepository.countBooksByPriceGreaterThan(15)).thenReturn(2);
-        assertEquals(2, bookService.countBooksByPriceGreaterThan(15));
-    }
+    // FINAL
 }
